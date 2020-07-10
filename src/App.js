9e66aa2524be0d3c8b9e2d066cd9565b134868ec
Gagan Singh <gagansingh.dev@outlook.com>
2020-07-10 15:29:24 -0400

    Implemented custom page routing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
-const items = [
+const accordionItems = [
    { title: "What is React?", content: "A front end JS framework" },
    {
       title: "Why use React?",
@@ -17,24 +19,35 @@ const items = [
    },
 ];
 
-const options = [
+const dropdownOptions = [
    { label: "The Color Red", value: "red" },
    { label: "The Color Green", value: "green" },
    { label: "The Shade of Blue", value: "blue" },
 ];
 
 export default () => {
+   const [selected, setSelected] = useState(dropdownOptions[0]);
+
    return (
       <div>
-         {/* <Accordion items={items} /> */}
-         {/* <Search /> */}
-
-         {/* <Dropdown
-            options={options}
-            selected={selected}
-            onSelectedChange={setSelected}
-         /> */}
-         <Translate />
+         <Header />
+         <Route path="/">
+            <Accordion items={accordionItems} />
+         </Route>
+         <Route path="/list">
+            <Search />
+         </Route>
+         <Route path="/dropdown">
+            <Dropdown
+               label="Select A Color"
+               options={dropdownOptions}
+               selected={selected}
+               onSelectedChange={setSelected}
+            />
+         </Route>
+         <Route path="/translate">
+            <Translate />
+         </Route>
       </div>
    );
 };
